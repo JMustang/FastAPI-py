d292c9a14188e8b0c932bd800f4f8259a019d084
Junior <eecfredes@gmail.com>
2022-10-04 22:04:36 -0300

    :sparkles: feat: Models - Post, DONE

--- app/main.py
+++ app/main.py
@@ -6,8 +6,20 @@ from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from . import models
+from .database import SessionLocal, engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 class Post(BaseModel):
