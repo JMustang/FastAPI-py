eb13999a6ccca8a3a5235c88951d0249db1a5208
Junior <eecfredes@gmail.com>
2022-10-07 22:38:42 -0300

    :sparkles: feat: created at, DONE

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
 
@@ -9,4 +10,6 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     author = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
