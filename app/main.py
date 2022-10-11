4852f9c5230f6457e990276ff227a88033ea0b8a
Junior <eecfredes@gmail.com>
2022-10-10 22:15:21 -0300

    :sparkles: feat: create get post by ID query with sqlalchemy, DONE

--- app/main.py
+++ app/main.py
@@ -96,9 +96,11 @@ def create_posts(post: Post, db: Session = Depends(get_db)):
 
 
 @app.get('/posts/{id}')
-def get_post(id: int):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
-    post = cursor.fetchone()
+def get_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
+    # post = cursor.fetchone()
+
+    post = db.query(models.Post).filter(models.Post.id == id).first()
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
