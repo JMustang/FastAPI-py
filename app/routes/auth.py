24297f5c8ffd1191ae1b719b1616dc99c0fb8e9b
Junior <eecfredes@gmail.com>
2022-10-24 21:52:38 -0300

    :sparkles: feat: user login, jwt token

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 
-from .. import database, schema, models
+from .. import database, schema, models, utils
 
 router = APIRouter(tags=['Authentication'])
 
@@ -12,5 +12,9 @@ def login(user_credentiols: schema.UserLogin, db: Session = Depends(database.get
         models.User.email == user_credentiols.email).first()
 
     if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f'Invalid Credentials')
+
+    if not utils.verify(user_credentiols.password, user.password):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f'Invalid Credentials')
