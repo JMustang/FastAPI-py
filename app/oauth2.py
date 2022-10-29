01fa5aad52da725fda74a4dbc5e26908bf435058
Junior <eecfredes@gmail.com>
2022-10-28 22:44:56 -0300

    :sparkles: feat: verify access token function, done

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,6 +1,8 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 
+from . import schema
+
 
 # SECRET_KEY
 # Algorithm
@@ -8,6 +10,8 @@ from datetime import datetime, timedelta
 
 # to get a string like this run:
 # openssl rand -hex 32
+
+# Never user your secret key inside the code
 SECRET_KEY = "5004136e79532ab2850f6204d1ce2162570f671ee37a8f647309ff4a3c919e75"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
@@ -22,3 +26,17 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithm=ALGORITHM)
+
+        id: str = payload.get('user_id')
+
+        if id is None:
+            raise credentials_exception
+        token_data = schema.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
