7b2895959aa5fab99fa419fd556e2fba463d22a9
Junior <eecfredes@gmail.com>
2022-10-28 22:31:55 -0300

    :sparkles: feat: schema - access token, done

--- app/schema.py
+++ app/schema.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from pydantic import BaseModel, EmailStr
+from typing import Optional
 
 
 class PostBase(BaseModel):
@@ -39,3 +40,12 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
