a7336cd60b6f329ddbc67b9ca9ae9a836cfcbe47
Junior <eecfredes@gmail.com>
2022-10-30 19:51:13 -0300

    :bug: fix: fixing oauth2 bugs

--- app/oauth2.py
+++ app/oauth2.py
@@ -24,10 +24,10 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({'exp': expire})
 
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=[ALGORITHM])
 
     return encoded_jwt
 
@@ -44,6 +44,8 @@ def verify_access_token(token: str, credentials_exception):
         token_data = schema.TokenData(id=id)
     except JWTError:
         raise credentials_exception
+
+    return token_data
 
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
