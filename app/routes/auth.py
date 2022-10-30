f0a8fc20f982826d37a616ea79582eea8c5d0418
Junior <eecfredes@gmail.com>
2022-10-30 19:52:54 -0300

    :bug: fix: fixing auth bugs

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -14,11 +14,11 @@ def login(user_credentiols: OAuth2PasswordRequestForm = Depends(), db: Session =
 
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f'Invalid Credentials')
+            status_code=status.HTTP_403_FORBIDDEN, detail=f'Invalid Credentials')
 
     if not utils.verify(user_credentiols.password, user.password):
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f'Invalid Credentials')
+            status_code=status.HTTP_403_FORBIDDEN, detail=f'Invalid Credentials')
 
     access_token = oauth2.create_access_token(data={'user_id': user.id})
 
