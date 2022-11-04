2c511f7017f21d0721878470e2500d31a493b060
Junior <eecfredes@gmail.com>
2022-11-03 22:08:13 -0300

    :sparkles: feat: foreignkey

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, Foreignkey
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
@@ -13,6 +13,8 @@ class Post(Base):
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
+    owner_id = Column(Integer, Foreignkey(
+        'users.id', ondelete='CASCADE'), nullable=False)
 
 
 class User(Base):
