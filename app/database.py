9a961af46fd990301606275bb629dfb888fe1393
Junior <eecfredes@gmail.com>
2022-11-14 21:28:41 -0300

    :books: docs: cleannig the code

--- app/database.py
+++ app/database.py
@@ -2,6 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+
 SQLALQUEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost/fastapi'
 
 engine = create_engine(SQLALQUEMY_DATABASE_URL)
@@ -16,3 +20,17 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# Future reference for database
+# while True:
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='fastapi',
+#                                 user='postgres', password='postgres', cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print('Database connection was succesfully established')
+#         break
+#     except Exception as error:
+#         print('connection to database is Failed!')
+#         print('Error: ', error)
+#         time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,42 +1,17 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from pydantic import BaseModel
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
 from .routes import post, user, auth
-
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-class Post(BaseModel):
-    title: str
-    author: str
-    published: bool = True
-
-
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastapi',
-                                user='postgres', password='postgres', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print('Database connection was succesfully established')
-        break
-    except Exception as error:
-        print('connection to database is Failed!')
-        print('Error: ', error)
-        time.sleep(2)
-
 
 app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(post.router)
 
 
-@app.get('/')
-def root():
-    return {'message': 'Hello, world!'}
+# @app.get('/')
+# def root():
+#     return {'message': 'Hello, world!'}
