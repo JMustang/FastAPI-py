7108115085d04222332e3c8562c19a3fafb6ae1f
Junior <eecfredes@gmail.com>
2022-11-17 21:56:19 -0300

    :sparkles: feat: .env config, almost done

--- app/database.py
+++ app/database.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 
-SQLALQUEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost/fastapi'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
-engine = create_engine(SQLALQUEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
