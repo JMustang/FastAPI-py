8ea9dbf42df54f6ebd8be36c6f3ee44166dae663
Junior <eecfredes@gmail.com>
2022-11-23 21:32:11 -0300

    :sparkles: feat: Left Join

--- app/routes/post.py
+++ app/routes/post.py
@@ -20,6 +20,10 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
     posts = db.query(models.Post).filter(
         models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
+    # Testing Left Join on python
+    results = db.query(models.Post).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True)
+
     # this line shows only post for the current user .filter(models.Post.owner_id == current_user.id).all
 
     return posts
