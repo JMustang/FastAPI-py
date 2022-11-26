314282eebde095b91923efdbe773da856ebacfbe
Junior <eecfredes@gmail.com>
2022-11-26 15:06:07 -0300

    :sparkles: feat: alembic

---
+++ alembic/versions/5bacc8f12492_create_posts_table.py
@@ -0,0 +1,24 @@
+"""create posts table
+
+Revision ID: 5bacc8f12492
+Revises: 
+Create Date: 2022-11-26 15:04:03.617297
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5bacc8f12492'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
