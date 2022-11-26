e50ee2796ff7611f5d9d33bb16d4d5c896074f18
Junior <eecfredes@gmail.com>
2022-11-25 21:11:56 -0300

    :sparkles: feat: Left Join - getPost, updatePost

--- app/routes/post.py
+++ app/routes/post.py
@@ -18,16 +18,17 @@ router = APIRouter(
 @router.get("/", response_model=List[schema.PostOut])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),
               limit: int = 10, skip: int = 0, search: Optional[str] = ''):
-    posts = db.query(models.Post).filter(
-        models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    # posts = db.query(models.Post).filter(
+    #     models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
     # Testing Left Join on python
-    results = db.query(models.Post, func.count(models.Vote.post_id).label('votes')).join(
-        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).all()
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label('votes')).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(
+        models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
     # this line shows only post for the current user .filter(models.Post.owner_id == current_user.id).all
 
-    return results
+    return posts
 
 
 # POST
@@ -47,10 +48,13 @@ def create_posts(post: schema.PostCreate,
 
 
 # GET BY ID
-@router.get('/{id}', response_model=schema.Post)
+@router.get('/{id}', response_model=schema.PostOut)
 def get_post(id: int, db: Session = Depends(get_db),
              current_user: int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    # post = db.query(models.Post).filter(models.Post.id == id).first()
+
+    post = db.query(models.Post, func.count(models.Vote.post_id).label('votes')).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
