f287b3f62e97a4468671718ebbd6dd9a82f83c4d
Junior <eecfredes@gmail.com>
2022-12-01 21:49:03 -0300

    :test_tube: test table users

--- alembic/versions/84e78d4d7ec4_add_user_table.py
+++ alembic/versions/84e78d4d7ec4_add_user_table.py
@@ -17,16 +17,15 @@ depends_on = None
 
 
 def upgrade():
-    op.create_table(
-        'users',
-        sa.Column('id', sa.Integer(), nullable=False),
-        sa.Column('email', sa.String(), nullable=False),
-        sa.Column('password', sa.String(), nullable=False),
-        sa.Column('created_at', sa.TIMESTAMP(timezone=True),
-                  server_default=sa.text('now()'), nullable=False),
-        sa.PrimaryKeyConstraint('id'),
-        sa.UniqueConstraint('email'),
-    )
+    op.create_table('users',
+                    sa.Column('id', sa.Integer(), nullable=False),
+                    sa.Column('email', sa.String(), nullable=False),
+                    sa.Column('password', sa.String(), nullable=False),
+                    sa.Column('created_at', sa.TIMESTAMP(timezone=True),
+                              server_default=sa.text('now()'), nullable=False),
+                    sa.PrimaryKeyConstraint('id'),
+                    sa.UniqueConstraint('email')
+                    )
     pass
 
 
