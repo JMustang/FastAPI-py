7fcc1ee5c8aca514b242f9fe8e1cc9155c5afcb3
Junior <eecfredes@gmail.com>
2022-12-04 21:05:15 -0300

    :sparkles: feat: alembic - migration | add 2 more column to posts table

---
+++ alembic/versions/dc80ac43e3b5_add_last_few_columns_to_posts_table.py
@@ -0,0 +1,32 @@
+"""add last few columns to posts table
+
+Revision ID: dc80ac43e3b5
+Revises: 9f906814ec34
+Create Date: 2022-12-04 20:56:31.127038
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dc80ac43e3b5'
+down_revision = '9f906814ec34'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('posts', sa.Column(
+        'published', sa.Boolean(), nullable=False, server_default='TRUE'
+    ),)
+    op.add_column('posts', sa.Column(
+        'created_at', sa.TIMESTAMP(timezone=True), nullable=False, server_default=sa.text
+        ('NOW(')),)
+    pass
+
+
+def downgrade():
+    op.drop_column('posts', 'published')
+    op.drop_column('posts', 'created_at')
+    pass
